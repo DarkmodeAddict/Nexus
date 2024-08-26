338f92b8198076f1fcd1a513a2836bca5dd5bbd6
Anurag <hamsen3880@gmail.com>
2024-08-26 13:50:09 +0530

    Build change V0.16.3

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
+echo "BUILD START"
+python3.9 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
-python3.9 manage.py collectstatic 
+python manage.py collectstatic --noinput
+echo "BUILD END"
